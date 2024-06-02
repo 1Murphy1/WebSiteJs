d398e1e45ec343fc5d2cea9582c1b59bdd0a9725
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2024-06-02 17:15:48 +0700

    Переход на след.стр
    -Создан файл с функцией авторизации и перехода на след. страницу

---
+++ login.js
@@ -0,0 +1,12 @@
+function showMain() {
+    var userInput = document.getElementById('login').value;
+    if (userInput.trim() === '') {
+        alert("Поле ввода пустое!");
+    } else {
+        var mainDiv = document.querySelector('.main');
+        mainDiv.style.display = 'block';
+        var mainDiv2 = document.querySelector('.login');
+        mainDiv2.style.display = 'none';
+        document.getElementById('NameUser').textContent = "Привет, " + userInput;
+    }
+}
